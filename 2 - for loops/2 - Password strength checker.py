ffcc910abca04278a0c0c6fdde45874e989b7603
Ling <cheesylingg@gmail.com>
2025-07-30 16:11:29 +0800

    Finished.

--- 2 - for loops/2 - Password strength checker.py
+++ 2 - for loops/2 - Password strength checker.py
@@ -3,6 +3,7 @@ A Strong password must be at least 8 characters long AND contain at least one up
 A Medium password meets some but not all of the criteria for a strong password.
 A Weak password is less than 8 characters long.
 """
+import sys
 
 strong = False
 medium = False
@@ -13,21 +14,27 @@ has_uppercase = False
 has_lowercase = False
 has_number = False
 
+
+
 def reset():
     global has_uppercase
     global has_lowercase
     global has_number
+    global weak
+    global medium
 
     has_uppercase = False
     has_lowercase = False
     has_number = False
+    weak = False
+    medium = False
 
 while True:
-
+    reset()
     password_input = input("Enter your password here or enter 'quit' to quit: ")
 
     if password_input == "quit":
-        break
+        sys.exit()
 
     pw_length = len(password_input)
     if pw_length < 8:
@@ -35,29 +42,28 @@ while True:
 
     if weak:
         print("Your password is weak! Try again.")
-        weak = False #I WAS STARING AT MY TERMINAL ON WHY ITS STILL WEAK AFTER INPUTTING MEDIUM PASSWORD, I FIGURED OUT THAT ITS ALWAYS TRUE AFTERWARDS T_T (kinda felt genius after it though :P)
+        reset() #weak = False -> reset() I WAS STARING AT MY TERMINAL ON WHY ITS STILL WEAK AFTER INPUTTING MEDIUM PASSWORD, I FIGURED OUT THAT ITS ALWAYS TRUE AFTERWARDS T_T (kinda felt genius after it though :P)
         continue
 
-
-
     for characters in password_input:
-
-        if characters.isupper:
+        if characters.isupper():
             has_uppercase = True
-        if characters.isdigit:
+        if characters.isdigit():
             has_number = True
-        if characters.islower:
+        if characters.islower():
             has_lowercase = True        
 
-
-
-
+    strong_status = has_lowercase and has_number and has_uppercase and not weak
+    if strong_status is True:
+        print("Your password is strong!")
+        sys.exit()
 
     if (has_uppercase or has_lowercase or has_number) and not weak:
         medium = True
-        reset()
 
     if medium:
         print("Your password is moderately weak! Try again.")
+        reset()
         continue
-    
+    else:
+        print("Try again!")
